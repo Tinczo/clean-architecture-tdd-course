e290e3e370e26ac53505aeee66e7755d54eb8cdf
Szymon Wieczorek <doszymona@gmail.com>
2026-01-06 22:09:51 +0100

    episode05: create contracts for data sources

---
+++ lib/features/number_trivia/data/datasources/number_trivia_local_datasource.dart
@@ -0,0 +1,12 @@
+import '../models/number_trivia_model.dart';
+
+abstract class NumberTriviaLocalDataSource {
+  /// Gets the cached [NumberTriviaModel] which was gotten the last time
+  /// the user had an internet connection.
+  ///
+  /// Throws a [CacheException] if no cached data is present.
+  Future<NumberTriviaModel> getLastNumberTrivia();
+
+  /// Caches the [NumberTriviaModel] locally.
+  Future<void> cacheNumberTrivia(NumberTriviaModel numberTrivia);
+}
